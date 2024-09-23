5d95502699aff37f1df49e756b4ac3b1b8b1e377
baopen753 <tranquocbao070503@gmail.com>
2024-09-23 16:06:05 +0700

    add: config files

---
+++ src/main/java/org/baopen753/bookingappbackend/BookingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.baopen753.bookingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/baopen753/bookingappbackend/BookingappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baopen753.bookingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
